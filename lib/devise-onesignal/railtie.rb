8afa67713cca04ede2a79fbe0a947fc6129cd1a6
Jonas Hübotter <jonas.huebotter@gmx.de>
2017-12-19 15:53:58 +0100

    minor bugfixes

--- lib/devise-onesignal/railtie.rb
+++ lib/devise-onesignal/railtie.rb
@@ -4,7 +4,7 @@ module DeviseOnesignal
     class Railtie < Rails::Railtie
 
         initializer 'devise-onesignal.initialize' do
-            abort 'Set your OneSignal app id in `config/initializers/devise-onesignal.rb`' if DeviseOnesignal.configuration && DeviseOnesignal.configuration.app_id.nil?
+            abort 'Set your OneSignal app id in `config/initializers/devise-onesignal.rb`' if DeviseOnesignal.configuration && ( DeviseOnesignal.configuration.app_id.nil? || DeviseOnesignal.configuration.app_id == '' )
             ActiveSupport.on_load :action_controller do
                 include DeviseOnesignal::DeviceConcern
             end
