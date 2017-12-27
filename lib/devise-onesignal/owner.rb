503e3d5f5dfecd442bb9e2e1a4fa55bc4dd27737
Jonas Hübotter <jonas.huebotter@gmx.de>
2017-12-27 09:58:14 +0100

    Create owner.rb

---
+++ lib/devise-onesignal/owner.rb
@@ -0,0 +1,27 @@
+module DeviseOnesignal
+    module Owner
+
+        def self.included base
+            base.extend ClassMethods
+        end
+
+        module ClassMethods
+            def has_devices
+                has_many :devices, as: :owner, dependent: :destroy
+                include DeviseOnesignal::Owner::InstanceMethods
+            end
+        end
+
+        module InstanceMethods
+
+            def onesignal_player_ids
+                onesignal_player_ids = []
+                self.devices.each do |device|
+                    onesignal_player_ids << device.onesignal_id
+                end
+            end
+
+        end
+
+    end
+end
